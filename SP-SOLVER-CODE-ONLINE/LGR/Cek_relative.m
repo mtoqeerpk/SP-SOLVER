294433b0e1d2ea7fc0c132b328150cf4a38be960
mutlaqalarouj <64172265+mutlaqalarouj@users.noreply.github.com>
2020-05-04 03:13:20 +0100

    Delete Cek_relative.m

--- SP-SOLVER-CODE-ONLINE/LGR/Cek_relative.m
+++
@@ -1,9 +0,0 @@
-function [Cr_ek] = Cek_relative(Swn, m_ek)
-    % Function to calculate relative EK coupling as a function of Swn
-    % where:    Cr      = relative coupling coefficient [-]
-    %           Swn     = normalised water saturation [-]
-    %           m       = exponent and/or coefficients of Swn in Cr
-    
-    Cr_ek=(Swn).^m_ek;
-    
-end
